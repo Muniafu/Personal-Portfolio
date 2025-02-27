37cee68c821a64e3bd9ae6523d907d31251c2fc0
Muniafu <elambaraka2019@gmail.com>
2025-02-27 13:13:53 +0300

    add authentication and Firebase service files

---
+++ portfolio/src/services/firebase.js
@@ -0,0 +1,22 @@
+/* Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBGdA80zvMHPX9u_aWFwvy0oGKb1J8TcIU",
+  authDomain: "personal-portfolio-mvp.firebaseapp.com",
+  projectId: "personal-portfolio-mvp",
+  storageBucket: "personal-portfolio-mvp.firebasestorage.app",
+  messagingSenderId: "42299448555",
+  appId: "1:42299448555:web:16e0fa059f3657cb4872ae",
+  measurementId: "G-6B3J28DDVX"
+};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+*/
